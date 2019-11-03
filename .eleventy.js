2c12b7b06fc888e97c6787ea450879744c37daba
Aina Requena <hello@ainarequena.com>
2019-11-03 19:24:23 +0100

    Initial commit

---
+++ .eleventy.js
@@ -0,0 +1,16 @@
+module.exports = function (eleventyConfig) {
+
+    eleventyConfig.addPassthroughCopy({ "src/images" : "images"});
+    eleventyConfig.addPassthroughCopy({ "src/admin" : "admin"});
+
+    return {
+        dir: {
+            input: "src/views",
+            output: "dist",
+            includes: "_includes/partials",
+            layouts: "_includes/layouts"
+        },
+        templateFormats: ["njk"],
+        passthroughFileCopy: true
+    };
+};
---
+++ postcss.config.js
@@ -0,0 +1,10 @@
+module.exports = ({
+    plugins: {
+        autoprefixer: { 
+            grid: 'autoplace'
+        },
+        cssnano: {
+            preset: 'default'
+        }
+    }
+})
